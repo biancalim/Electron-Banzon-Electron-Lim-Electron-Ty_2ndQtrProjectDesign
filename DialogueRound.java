74f08b6925b338a5af995256b98c7e05994fc761
biancalim <150140044+biancalim@users.noreply.github.com>
2024-01-28 12:04:02 +0800

    Update DialogueRound.java

---
+++ DialogueRound.java
@@ -0,0 +1,17 @@
+package aloverorafighter;
+
+import java.util.*;
+
+public class DialogueRound {
+    
+    private ArrayList<Dialogue> dialogueChoices;
+    
+    public void setDialogueChoices(ArrayList<Dialogue> newDialogueChoices) {
+        this.dialogueChoices = newDialogueChoices;
+    }
+    
+    public ArrayList<Dialogue> getDialogueChoices() {
+        return this.dialogueChoices;
+    }
+    
+}
